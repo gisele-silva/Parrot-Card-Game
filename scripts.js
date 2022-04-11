6fef2b58484928a315f9b15404c9733dce435196
gisele <giselesilva2311@gmail.com>
2022-04-11 17:43:09 -0300

    primeira versao js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,104 @@
-alert("A quantidade de cartas deve ser um número par entre 4 e 14")
+alert("Para jogar Parrot você deve informar um quantidade par de cartas entre 4 e 14")
 quantidadeCartas = Number(prompt("Informe a quantidade de cartas"))
 
-while (quantidadeCartas < 4 || quantidadeCartas === 5 || quantidadeCartas === 7 || quantidadeCartas === 9 || quantidadeCartas === 11 || quantidadeCartas >= 13) {
+while (quantidadeCartas < 4 || quantidadeCartas % 2 !== 0 || quantidadeCartas >= 14) {
     alert("Quantidade inválida")
     quantidadeCartas = Number(prompt("Informe a quantidade de cartas"))
 }
-alert("Bem vindo")
+alert("Bem vindo ao Parrot Card Game")
 
 if (quantidadeCartas === 4) {
     document.querySelector(".jogo4").classList.remove("apaga")
+    const cartas = document.querySelector(".jogo4");
+    const imagem = [
+        'bobrossparrot.gif',
+        'explodyparrot.gif'
+    ];
+    let cartaHTML = '';
+    imagem.forEach(imagens =>{
+        cartaHTML += `
+        <div> 
+            <img src="img/${imagens}">
+            
+        </div>
+        `
+    })  
+
 } else if (quantidadeCartas === 6) {
     document.querySelector(".jogo6").classList.remove("apaga")
+    const cartas = document.querySelector(".jogo6");
+    const imagem = [
+        'bobrossparrot.gif',
+        'explodyparrot.gif',
+        'fiestaparrot.gif'
+    ];
+    let cartaHTML = '';
+    imagem.forEach(imagens =>{
+        cartaHTML += `
+        <div> 
+            <img src="img/${imagens}">
+            
+        </div>
+        `;
+    });
+    
+    
 } else if (quantidadeCartas === 8) {
-    document.querySelector(".jogo8").classList.remove("apaga")
+    document.querySelector(".jogo8").classList.remove("apaga");
+    const cartas = document.querySelector(".jogo8");
+    const imagem = [
+        'bobrossparrot.gif',
+        'explodyparrot.gif',
+        'fiestaparrot.gif',
+        'metalparrot.gif'
+    ];
+    let cartaHTML = '';
+    imagem.forEach(imagens =>{
+        cartaHTML += `
+        <div> 
+            <img src="img/${imagens}">
+            
+        </div>
+        `
+    })  
 } else if (quantidadeCartas === 10) {
     document.querySelector(".jogo10").classList.remove("apaga")
+    const cartas = document.querySelector(".jogo10");
+    const imagem = [
+        'bobrossparrot.gif',
+        'explodyparrot.gif',
+        'fiestaparrot.gif',
+        'metalparrot.gif',
+        'revertitparrot.gif'
+    ];
+    let cartaHTML = '';
+    imagem.forEach(imagens =>{
+        cartaHTML += `
+        <div> 
+            <img src="img/${imagens}">
+            
+        </div>
+        `
+    })  
 } else if (quantidadeCartas === 12) {
     document.querySelector(".jogo12").classList.remove("apaga")
+    const cartas = document.querySelector(".jogo12");
+    const imagem = [
+        'bobrossparrot.gif',
+        'explodyparrot.gif',
+        'fiestaparrot.gif',
+        'metalparrot.gif',
+        'revertitparrot.gif',
+        'tripletsparrot.gif'
+    ];
+    let cartaHTML = '';
+    imagem.forEach(imagens =>{
+        cartaHTML += `
+        <div> 
+            <img src="img/${imagens}">
+            
+        </div>
+        `
+    })  
 }
+
